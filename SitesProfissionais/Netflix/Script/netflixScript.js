91dcfadc5d57453edff80be55aaae298300607a9
mig1998 <miguelaras@hotmail.com>
2021-07-04 18:31:52 -0300

    new

---
+++ SitesProfissionais/Netflix/Script/netflixScript.js
@@ -0,0 +1,11 @@
+function ShowInfo(img){
+document.getElementById('info').style.display='block';
+document.getElementById('imgInfo').style.display='block';
+
+
+document.getElementById('imgShow').src=img;
+
+
+
+
+}
