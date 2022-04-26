7d8763136098cf0a0b6a49eddce3d5a86549ce91
mig1998 <miguelaras@hotmail.com>
2022-04-26 05:08:28 -0300

    Ok

--- assets/scripts/inicio.js
+++ assets/scripts/inicio.js
@@ -2,9 +2,21 @@
 var divMyText = document.getElementById("divMyText")
 
 
-const sou = "Ola, ,s,o,u, ,M,i,g,u,e,l, ,e,u, ,S,o,u, ,D,e,s,e,n,v,o,l,v,e,d,o,r";
 
-const color = ['white','cyan'];
+
+var sectionLayouts = document.getElementById("layouts")
+var sectionApi = document.getElementById("consumo")
+var sectionAndroid = document.getElementById("android")
+var sectionc = document.getElementById("c")
+var sectionJava = document.getElementById("java")
+
+
+
+
+
+const sou = "Ola, ,s,o,u, ,M,i,g,u,e,l,<br> ,e,u, ,S,o,u, ,D,e,s,e,n,v,o,l,v,e,d,o,r";
+
+const color = ['lightgreen','yellowgreen','green'];
 
 var arraysou = sou.split(",");
 
@@ -21,14 +33,31 @@ for (let i = 0; i < arraysou.length; i++) {
     myText.innerHTML = letra;
     myText.id = "myText" + i;
     myText.style.display = 'inline';
-    myText.style.color = color[num];
-
     myText.style.textShadow = "0px 0px 33px " + color[num];
     divMyText.append(myText);
 
 }
 
 
+function changeText(){
+
+    for (let i = 0; i < arraysou.length; i++) {
+
+        let text = document.querySelectorAll('#myText' + i);
+    
+        let num = Math.floor(Math.random() * color.length);
+   
+        text.forEach(element => {
+            element.style.color=color[num];
+          
+        });
+
+  
+ 
+
+    }
+
+}
 
 
 function changeSection(section) {
@@ -36,17 +65,13 @@ function changeSection(section) {
     let session = document.getElementById(section)
 
     if (session.style.display == 'none') {
-        session.style.display = 'block';
+        session.style.display = 'block'; 
+    }else{
+        session.style.display = 'none'; 
     }
+
+
 
 
 }
 
-
-function hideSession(section){
-    let session = document.getElementById(section)
-
-    if (session.style.display == 'block') {
-        session.style.display = 'none';
-    }
-}
